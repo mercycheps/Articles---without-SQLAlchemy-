ba07fba2ab22cee73bad1653029fef259d39c9ba
Mercy <maritimmercy8@gmail.com>
2025-05-25 21:30:32 +0300

    created database

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,8 +1,10 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+
+
 def get_connection():
  conn = psycopg2.connect(
-"dbname=articles_challenge user=your_username password=your_password"
+  "dbname=articles_challenge user=your_username password=your_password"
 )
  conn.cursor_factory = RealDictCursor # This enables column access by name
  return conn
